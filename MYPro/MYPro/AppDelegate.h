924f5738aeb1837ad9c7a1e9159c7931c8c8e723
layerLP <xy880509@163.com>
2017-06-08 18:43:26 +0800

    init pro

---
+++ MYPro/MYPro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MYPro
+//
+//  Created by woyoushmy8 on 2017/6/8.
+//  Copyright © 2017年 woyoushmy8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MYPro/MYPro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MYPro
+//
+//  Created by woyoushmy8 on 2017/6/8.
+//  Copyright © 2017年 woyoushmy8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
