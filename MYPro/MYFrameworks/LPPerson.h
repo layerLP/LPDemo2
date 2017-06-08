01fa8d9244c987015d837c6f4e83b274c57fc3b8
layerLP <xy880509@163.com>
2017-06-08 18:48:28 +0800

    create frameworks

---
+++ MYPro/MYFrameworks/LPPerson.h
@@ -0,0 +1,14 @@
+//
+//  LPPerson.h
+//  MYPro
+//
+//  Created by woyoushmy8 on 2017/6/8.
+//  Copyright © 2017年 woyoushmy8. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LPPerson : NSObject
+
+-(void)eat;
+@end
---
+++ MYPro/MYFrameworks/LPPerson.m
@@ -0,0 +1,17 @@
+//
+//  LPPerson.m
+//  MYPro
+//
+//  Created by woyoushmy8 on 2017/6/8.
+//  Copyright © 2017年 woyoushmy8. All rights reserved.
+//
+
+#import "LPPerson.h"
+
+@implementation LPPerson
+
+- (void)eat {
+    NSLog(@"正在吃");
+}
+
+@end
